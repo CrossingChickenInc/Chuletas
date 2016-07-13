a16b16b29d9dcf34962603379f95a57111a66893
Allan Leon <aleon.107@outlook.com>
2016-07-12 22:15:13 -0400

    Initial commit

---
+++ game/states/boot.js
@@ -0,0 +1,20 @@
+	var States = {};
+
+// Loads all the necesasary assets before starting the game.
+States.Boot = {
+	// Loads the assets.
+	preload: function(){
+		this.game.load.image('dukeCollider', 'assets/images/characters/collider.png');
+		this.game.load.image('blockH', 'assets/images/backgrounds/blockH.png');
+		this.game.load.image('block', 'assets/images/backgrounds/block.png');
+		this.game.load.spritesheet('duke', 'assets/images/characters/duke.png', 23, 43);
+		
+		this.game.load.image('map', 'assets/images/backgrounds/yourmommasmap.png');
+
+		this.game.load.spritesheet('portal', 'assets/images/misc/portal.png', 123, 123);
+	},
+	// Starts the next state.
+	create: function(){
+		this.state.start('Play');
+	}
+};
---
+++ game/states/play.js
@@ -0,0 +1,44 @@
+// Principal game state, all the fun begins here.
+States.Play = {
+	// Sets the game's basic configurations.
+	init: function(){
+		this.game.renderer.renderSession.roundPixels = true;
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+	},
+	// Starts the game.
+	create: function(){
+		// Sets the game background with file associated to 'map' on boot.js.
+		this.game.background = this.game.add.sprite(0, 0, 'map');
+
+		// Sets the world bounds.
+		this.game.world.setBounds(0, 0, 1536, 1536);
+
+		// Initialiazes the cursor keys.
+		this.game.cursors = game.input.keyboard.createCursorKeys();
+
+		// Creates and loads a Player object.
+    	game.player = new Player(window.game);
+		game.player.load();
+		game.physics.arcade.enable(game.player);
+
+		// Sets the camera to follow the player.
+		this.game.camera.follow(this.game.player.colliderSprite);
+
+		//Creates and loads blocks
+		game.obstacle = new Obstacle(window.game, 'play');
+		game.obstacle.load();
+		
+		game.portals = [];
+
+		game.portals.push(new Portal(window.game, 50, 280, 'Play')); //Vale
+		game.portals.push(new Portal(window.game, 230, 180, 'Play')); //Robe
+		game.portals.push(new Portal(window.game, 400, 1000, 'Play')); //Fabio
+	},
+	// Updates all the game's objects.
+	update: function(){
+		// Updates the player.
+		game.player.update(game.player);
+		game.portals.every(portal => portal.update());
+	}
+
+};
