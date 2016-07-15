5108797a4951a36230e524111900485c33e4ed1d
Sergio Oporto <Sergio.E.Oporto@ieee.org>
2016-07-14 23:12:11 -0400

    Added a new state for the Math Bombs' minigame

---
+++ game/states/mathbombs.js
@@ -0,0 +1,16 @@
+// Math Bombs minigame state
+States.MathBombs = {
+	// Sets the game's basic configurations.
+	init: function(){
+		this.game.renderer.renderSession.roundPixels = true;
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+	},
+	// Starts the game.
+	create: function(){
+		
+	},
+	// Updates all the game's objects.
+	update: function(){
+	}
+
+};
