e3b7c1ab582464c198dadd07eb80257c61acf669
Vanessa Sanjinez <vane.sanji@gmail.com>
2016-07-18 10:50:15 -0400

    Separated createBombs function

--- game/states/mathbombs.js
+++ game/states/mathbombs.js
@@ -1,3 +1,30 @@
+function createBombs() {
+	var bombs;
+	//Group for bombs
+	bombs = this.game.add.group();
+	bombs.enableBody = true;
+	//Creation of bombs
+	for (var i = 0; i < 4; i++) {
+
+		//Random var for creation of bombs (not all in the same position)
+		var random = this.game.rnd.realInRange(50, 500);
+		var bomb = bombs.create(0, random, 'bomb');
+		//var bomb = this.game.create(0, random, 'bomb');
+			
+		//Little modification to bombs size
+		bomb.width = 30;
+		bomb.height = 150;
+
+		//  Gravity on x axis
+        bomb.body.gravity.x = 300;
+        bomb.body.collideWorldBounds = true;
+
+        //Another random var for velocity this could be used for levels 
+        var randomVelocity = this.game.rnd.realInRange(110,60);
+        bomb.body.velocity.x = randomVelocity;
+	};
+}
+
 // Math Bombs minigame state
  States.MathBombs = {
  	// Sets the game's basic configurations.
@@ -5,43 +32,14 @@
  		this.game.renderer.renderSession.roundPixels = true;
  		this.physics.startSystem(Phaser.Physics.ARCADE);
  	},
-
- 	createBombs: function(){
- 		var bombs;
- 		//Group for bombs
- 		bombs = this.game.add.group();
- 		bombs.enableBody = true;
- 		//Creation of bombs
- 		//for (var i = 0; i < 4; i++) {
-
- 			//Random var for creation of bombs (not all in the same position)
- 			var random = this.game.rnd.realInRange(50, 500);
-			//var bomb = bombs.create(0, random, 'bomb');
-			var bomb = this.game.create(0, random, 'bomb');
-			
-			//Little modification to bombs size
-			bomb.width = 30;
-			bomb.height = 150;
-
-			//  Gravity on x axis
-	        bomb.body.gravity.x = 300;
-	        bomb.body.collideWorldBounds = true;
-
-	        //Another random var for velocity this could be used for levels 
-	        var randomVelocity = this.game.rnd.realInRange(110,60);
-	        bomb.body.velocity.x = randomVelocity;
-
- 		//};
- 	}
  	// Starts the game.
  	create: function(){
  		//this.game.stage.backgroundColor = 'rgb(204, 255, 204)';
  		this.game.stage.backgroundColor = 'rgb(255, 255, 255)';
- 		this.game.events.loop(Phaser.Timer.SECOND, createBombs, this);
+ 		this.game.time.events.loop(Phaser.Timer.SECOND, createBombs, this);
  	
  	},
  	// Updates all the game's objects.
  	update: function(){
-
  	}
  };
