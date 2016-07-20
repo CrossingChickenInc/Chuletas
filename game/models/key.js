956c0acebe3e7319168673f3342105f94d6e5918
Jhonatan Montaño <jhoarmogui@gmail.com>
2016-07-20 17:41:28 -0400

    adding random letter

--- game/models/key.js
+++ game/models/key.js
@@ -1,6 +1,5 @@
 // Declaration of Key data.
-//var keys;
-//var pmap = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
 function Key(game){
 	this.game = game;
 	this.sprite = null;
@@ -32,8 +31,9 @@ Key.prototype.render = function(){
 	//this.colliderSprite.immovable = true; // makes it immovable when a collision occurs
 	this.colliderSprite.body.collideWorldBounds = true; // colliderSprite cannot exceed the world bounds
 
-	this.game.add.text(this.x + 45, this.y + 15, this.pmap.indexOf(this.keyNumber), { font: "50px Courier", fill: "#000", tabs: 32 });
-	//createString();
+	//adding the random letter
+	this.game.add.text(this.x + 45, this.y + 15, this.pmap[this.keyNumber], { font: "50px Courier", fill: "#000", tabs: 32 });
+
 	//createEventListeners();
 };
 
@@ -44,77 +44,7 @@ Key.prototype.update = function(){}
 	//this.setBodyPosition(this.colliderSprite.x - this.positionData.colliderDifference.x, this.colliderSprite.y - this.positionData.colliderDifference.x);
 
 
-function createEventListeners() {
 
-    keys = game.input.keyboard.addKeys(
-        {
-
-            'key0': Phaser.Keyboard.A,
-            'key1': Phaser.Keyboard.B,
-            'key2': Phaser.Keyboard.C,
-            'key3': Phaser.Keyboard.D,
-            'key4': Phaser.Keyboard.E,
-            'key5': Phaser.Keyboard.F,
-            'key6': Phaser.Keyboard.G,
-            'key7': Phaser.Keyboard.H,
-            'key8': Phaser.Keyboard.I,
-            'key9': Phaser.Keyboard.J,
-            'key10': Phaser.Keyboard.K,
-            'key11': Phaser.Keyboard.L,
-            'key12': Phaser.Keyboard.M,
-            'key13': Phaser.Keyboard.N,
-            'key14': Phaser.Keyboard.O,
-            'key15': Phaser.Keyboard.P,
-            'key16': Phaser.Keyboard.Q,
-            'key17': Phaser.Keyboard.R,
-            'key18': Phaser.Keyboard.S,
-            'key19': Phaser.Keyboard.T,
-            'key20': Phaser.Keyboard.U,
-            'key21': Phaser.Keyboard.V,
-            'key22': Phaser.Keyboard.W,
-            'key23': Phaser.Keyboard.X,
-            'key24': Phaser.Keyboard.Y,
-            'key25': Phaser.Keyboard.Z
-        }
-    );
-
-    for (var i = 0; i < 26; i++)
-    {
-        keys['key' + i].onDown.add(keyDown, this, 0, i);
-    }
-}
-
-//
-function createString(){
-	
-	//this.game.add.text(this.x+12, this.y+9, pmap.join("\t"), { font: "14px Courier", fill: "#000", tabs: 32 });
-   // this.game.add.text(this.x + 45,this.y + 15, this.pmap.join("\t"), { font: "50px Courier", fill: "#ffff00", tabs: 32 });
-
-}
-
-//
-function keyDown() {
-
-    if (typeof p !== 'undefined')
-    {
-        //  It came from a Keyboard Event, in which case the color index is in p, not i.
-        i = p;
-    }
-
-    if (i < 0)
-    {
-        i = 15;
-    }
-    else if (i >= 16)
-    {
-        i = 0;
-    }
-
-    colorIndex = i;
-    color = game.create.palettes[palette][pmap[colorIndex]];
-
-
-}
 
 
 
