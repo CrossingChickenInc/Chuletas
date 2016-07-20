14d9ff4a0493b2c2cbc4abb25d3d29d9f2a0271b
Jhonatan Montaño <jhoarmogui@gmail.com>
2016-07-20 12:00:36 -0400

    combo codes state

---
+++ game/stages/combocodes.js
@@ -0,0 +1,36 @@
+function ComboCodes() {}
+
+/**
+ * Sets the state's basic configurations.
+ */
+ComboCodes.prototype.init = function() {
+	this.game.renderer.renderSession.roundPixels = true;
+	this.physics.startSystem(Phaser.Physics.ARCADE);
+};
+
+/**
+ * Initializes the stage objects.
+ */
+
+ComboCodes.prototype.create = function() {
+
+	// Sets the world bounds.
+	this.game.world.setBounds(0, 0, 800, 600);
+
+	// Creates and loads a Key object.
+	this.game.Key = new Key(window.game);
+	this.game.Key.render();
+	//this.game.Key.setBodySprite(50,50);
+
+}	
+
+/**
+ * Updates all the stages's objects.
+ */
+ComboCodes.prototype.update = function(){
+
+	this.game.Key.update();
+}
+
+// Adds this stage to the game's states.
+States.ComboCodes = new ComboCodes();
