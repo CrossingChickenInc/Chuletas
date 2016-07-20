8fb0f6636a017e80fa37e02e4a80829202a43f94
Jhonatan Montaño <jhoarmogui@gmail.com>
2016-07-20 11:59:55 -0400

    Key Script

---
+++ game/models/key.js
@@ -0,0 +1,55 @@
+// Declaration of Key data.
+//var keys;
+var pmap = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+function Key(game){
+	this.game = game;
+	this.sprite = null;
+	this.colliderSprite = null;
+	this.x = game.rnd.between(0, 750);
+    this.y = game.rnd.between(0, 550);
+    this.keyNumber = game.rnd.between(0,25);
+	this.enableBody = true;
+	this.positionData = {
+	colliderDifference: {x: 4, y: 3}, // distance from collider sprite to sprite
+};
+
+	this.keys;
+	//this.pmap = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+}
+
+// Initializes the Keys sprites.
+Key.prototype.render = function(){
+	// loads sprites
+	this.colliderSprite = this.game.add.sprite(this.x - this.positionData.colliderDifference.x, this.y - this.positionData.colliderDifference.y, 'KeyCollider');
+	this.sprite = this.game.add.sprite(this.x, this.y, 'KeyBackground');
+	this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+
+	// sets sprite properties
+	this.colliderSprite.alpha = 0; // invisible collider sprite
+	this.game.physics.arcade.enable(this.colliderSprite); // enables physics on colliderSprite
+	this.game.physics.arcade.enable(this.sprite); // enables physics on sprite
+
+	//this.colliderSprite.immovable = true; // makes it immovable when a collision occurs
+	this.colliderSprite.body.collideWorldBounds = true; // colliderSprite cannot exceed the world bounds
+
+	this.game.add.text(this.x + 45, this.y + 15, this.pmap, { font: "50px Courier", fill: "#000", tabs: 32 });
+	//createString();
+	//createEventListeners();
+};
+
+
+// Updates the Key.
+Key.prototype.update = function(){
+	
+	//this.setBodyPosition(this.colliderSprite.x - this.positionData.colliderDifference.x, this.colliderSprite.y - this.positionData.colliderDifference.x);
+
+// Set position for te key
+Key.prototype.setBodySprite = function(x, y) {
+	this.colliderSprite.x = x;
+	this.colliderSprite.y = y;
+}
+
+
+
+
+
