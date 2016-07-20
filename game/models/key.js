cba66fb58abcd8000ceac4cee07d75ebaaa53803
Jhonatan Montaño <jhoarmogui@gmail.com>
2016-07-20 17:59:59 -0400

    adding keysEventsListeners

--- game/models/key.js
+++ game/models/key.js
@@ -39,11 +39,73 @@ Key.prototype.render = function(){
 
 
 // Updates the Key.
-Key.prototype.update = function(){}
-	
+Key.prototype.update = function(){
 	//this.setBodyPosition(this.colliderSprite.x - this.positionData.colliderDifference.x, this.colliderSprite.y - this.positionData.colliderDifference.x);
+}
+	
+	
 
+function createEventListeners() {
 
+    keys = game.input.keyboard.addKeys(
+        {
+
+            'key0': Phaser.Keyboard.A,
+            'key1': Phaser.Keyboard.B,
+            'key2': Phaser.Keyboard.C,
+            'key3': Phaser.Keyboard.D,
+            'key4': Phaser.Keyboard.E,
+            'key5': Phaser.Keyboard.F,
+            'key6': Phaser.Keyboard.G,
+            'key7': Phaser.Keyboard.H,
+            'key8': Phaser.Keyboard.I,
+            'key9': Phaser.Keyboard.J,
+            'key10': Phaser.Keyboard.K,
+            'key11': Phaser.Keyboard.L,
+            'key12': Phaser.Keyboard.M,
+            'key13': Phaser.Keyboard.N,
+            'key14': Phaser.Keyboard.O,
+            'key15': Phaser.Keyboard.P,
+            'key16': Phaser.Keyboard.Q,
+            'key17': Phaser.Keyboard.R,
+            'key18': Phaser.Keyboard.S,
+            'key19': Phaser.Keyboard.T,
+            'key20': Phaser.Keyboard.U,
+            'key21': Phaser.Keyboard.V,
+            'key22': Phaser.Keyboard.W,
+            'key23': Phaser.Keyboard.X,
+            'key24': Phaser.Keyboard.Y,
+            'key25': Phaser.Keyboard.Z
+        }
+    );
+
+    for (var i = 0; i < 26; i++)
+    {
+        keys['key' + i].onDown.add(keyDown, this, 0, i);
+    }
+}
+
+//
+function keyDown(i,p) {
+
+    if (typeof p !== 'undefined')
+    {
+        //  It came from a Keyboard Event, in which case the color index is in p, not i.
+        i = p;
+    }
+
+    if (i < 0)
+    {
+        i = 25;
+    }
+    else if (i >= 26)
+    {
+        i = 0;
+    }
+
+    this.game.add.text(10, 10, "INPUT VERIFICATION", { font: "50px Courier", fill: "#FFD700", tabs: 32 });
+
+}
 
 
 
